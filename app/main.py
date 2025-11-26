2f3a6fb9c597c68d7f76e8cdaeb430f5ad94e5b7
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-11-26 17:40:35 +0100

    context manager

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        open(self.filename, "w").close()
+        return self
+
+    def __exit__(self, exc_type: None, exc_val: None, exc_tb: None) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+
